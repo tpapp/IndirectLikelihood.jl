a30e92a73fa5574ec913d8d13c04445732a5584d
Tamas K. Papp <tkpapp@gmail.com>
2017-09-14 16:26:42 +0200

    Added tests for weighted MvNormal.

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,26 @@ using Distributions
     end
 end
 
+@testset "MvNormalSS summary statistics and ML (weighted)" begin
+    for _ in 1:100
+        K = rand(5:10)
+        N = K + rand(10:100)
+        X = randn(N, K)
+        wv = Weights(abs.(randn(N)))
+        W = sum(wv)
+        mvn = summary_statistics(MvNormalSS, X, wv)
+        @test mvn.W == W
+        @test size(mvn.m) == (K, )
+        @test mvn.m ≈ vec(mean(X, wv, 1))
+        Xc = X .- mvn.m'
+        @test mvn.S ≈ ((values(wv).*Xc)'*Xc)/W
+        @test ML(mvn) == MvNormalParams(mvn.m, mvn.S)
+        @test size(mvn) == (W, K)
+        @test repr(mvn) ==
+            "Summary statistics for multivariate normal, $(sum(wv)) × $(K) samples"
+    end
+end
+
 @testset "MvNormalSS log likelihood" begin
     for _ in 1:100
         K = rand(5:10)
