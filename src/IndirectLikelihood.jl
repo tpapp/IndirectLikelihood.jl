86381d18810dbef9b5636c8e03c94a22a0205683
Tamas K. Papp <tkpapp@gmail.com>
2017-12-04 21:30:00 +0100

    Added OLS.

--- src/IndirectLikelihood.jl
+++ src/IndirectLikelihood.jl
@@ -14,17 +14,8 @@ export
     # specific models
     MvNormalSS, MvNormalParams
 
-######################################################################
+
 # general interface
-######################################################################
-
-"""
-    ML(summary_statistics)
-
-Return the maximum likelihood of the parameters using the given
-`summary_statistics`. See `loglikelihood`.
-"""
-function ML end
 
 """
     summary_statistics(::Type{T}, data...)
@@ -35,11 +26,31 @@ interpretation of `data` depends on `T`.
 function summary_statistics end
 
 """
+    loglikelihood(summary_statistics, parameters)
+
+Return the log likelihood of the data with the given `summary_statistics` under
+and `parameters`, which come from the same model family.
+
+`loglikelihood(ss, ML(ss))` maximizes the loglikelihood for given summary
+statistics `ss`.
+"""
+function loglikelihood end
+
+"""
+    ML(summary_statistics)
+
+Return the maximum likelihood of the parameters using the given
+`summary_statistics`. See `loglikelihood`.
+"""
+function ML end
+
+"""
     indirect_loglikelihood(y, x)
 
 1. estimate the model from summary statistics `x` using maximum likelihood,
 
-2. return the likelihood of summary_statistics `y` under the estimated parameters.
+2. return the likelihood of summary_statistics `y` under the estimated
+parameters.
 
 Useful for pseudo-likelihood indirect inference, where `y` would be the observed
 and `x` the simulated data. See in particular
@@ -54,9 +65,8 @@ and `x` the simulated data. See in particular
 """
 indirect_loglikelihood(y, x) = loglikelihood(y, ML(x))
 
-######################################################################
+
 # model: multivariate normal
-######################################################################
 
 """
     is_conformable_square(vector, matrix)
@@ -131,15 +141,6 @@ end
 
 ML(ss::MvNormalSS) = MvNormalParams(ss.m, ss.S)
 
-"""
-    loglikelihood(summary_statistics, parameters)
-
-Return the log likelihood of the data with the given `summary_statistics` under
-and `parameters`, which come from the same model family.
-
-`loglikelihood(ss, ML(ss))` maximizes the loglikelihood for given summary
-statistics `ss`.
-"""
 function loglikelihood(ss::MvNormalSS, params::MvNormalParams)
     @unpack W, m, S = ss
     @unpack μ, Σ = params
@@ -148,6 +149,89 @@ function loglikelihood(ss::MvNormalSS, params::MvNormalParams)
     C = cholfact(Σ)
     d = m-μ
     -W/2*(K*log(2*π) + logdet(C) + dot(vec(S) + vec(d * d'), vec(inv(Σ))))
+end
+
+
+# OLS
+
+"""
+    OLS_data(Y, X)
+
+Ordinary least squares with dependent variable `Y` and design matrix `X`.
+
+Either
+
+1. `Y` is an ``n×m`` matrix, then ``Y = X B + E`` where `X` is a ``n×k`` matrix,
+`B` is a ``k×m`` parameter matrix, and ``Eᵢ ∼ N(0, Σ)`` is IID with ``m×m``
+variance matrix ``Σ`` (multivariate linear regression), or
+
+2. `Y` is a length `n` vector, then ``Y = X β + ϵ``, where `X` is a ``n×k``
+matrix, `β` is a parameter vector of `k` elements, and `ϵᵢ ∼ N(0, σ)` where `σ`
+is the variance of the normal error.
+
+See [`OLS_params`](@ref) for the parameters.
+"""
+struct OLS_data{TY <: Union{AbstractMatrix, AbstractVector},
+                TX <: AbstractMatrix}
+    Y::TY
+    X::TX
+    function OLS_data(Y::TY, X::TX) where {TY, TX}
+        @argcheck size(Y, 1) == size(X, 1)
+        new{TY, TX}(Y, X)
+    end
+end
+
+"""
+    OLS_params(B, Σ)
+
+Maximum likelihood estimated parameters for an OLS regression. See
+[`OLS_data`](@ref).
+"""
+struct OLS_params{TB <: Union{AbstractMatrix, AbstractVector},
+                  TΣ <: Union{<:Real, AbstractMatrix}}
+    B::TB
+    Σ::TΣ
+    function OLS_params(B::TB, Σ::TΣ) where {TB <: AbstractVector, TΣ <: Real}
+        new{TB, TΣ}(B, Σ)
+    end
+    function OLS_params(B::TB, Σ::TΣ) where {TB <: AbstractMatrix,
+                                             TΣ <: AbstractMatrix}
+        m = size(B, 2)
+        @argcheck size(Σ) == (m, m)
+        new{TB, TΣ}(B, Σ)
+    end
+end
+
+function ML(data::OLS_data)
+    @unpack Y, X = data
+    B = qrfact(X) \ Y
+    E = Y - X*B
+    Σ = E'*E / size(X, 1)
+    OLS_params(B, Σ)
+end
+
+function loglikelihood(data::OLS_data{TY}, params::OLS_params{TB, TΣ}) where
+    {TY <: AbstractVector, TB <: AbstractVector, TΣ <: Real}
+    @unpack Y, X = data
+    @unpack B, Σ = params
+    n = length(Y)
+    @argcheck size(X, 1) == n
+    @argcheck size(X, 2) == length(B)
+    E = Y - X * B
+    -0.5 * (n*(log(2*π) + log(Σ)) + sum(abs2, E)/Σ)
+end
+
+function loglikelihood(data::OLS_data{TY}, params::OLS_params{TB, TΣ}) where
+    {TY <: AbstractMatrix, TB <: AbstractMatrix, TΣ <: AbstractMatrix}
+    @unpack Y, X = data
+    @unpack B, Σ = params
+    n, m = size(Y)
+    @argcheck size(B, 1) == size(X, 2) # k
+    @argcheck size(B, 2) == m
+    E = Y - X * B
+    U = chol(Σ)
+    A = E / U
+    -0.5 * (n*(m*log(2*π) + 2*logdet(U)) + sum(abs2, A))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using IndirectLikelihood
+import IndirectLikelihood:
+    OLS_data, OLS_params
 using Base.Test
 using StatsBase
 using Distributions
@@ -53,5 +55,49 @@ end
         ℓ = sum(logpdf(MvNormal(paramsX.μ, paramsX.Σ), Y'))
         @test ℓ ≈ loglikelihood(mvnY, paramsX)
         @test ℓ ≈ indirect_loglikelihood(mvnY, mvnX)
+    end
+end
+
+@testset "OLS scalarvariate" begin
+    for _ in 1:100
+        k = rand(5:10)
+        n = rand(90:100) + k
+        X = randn(n, k)
+        β = randn(k)
+        Y = X * β + randn(n)
+        data = OLS_data(Y, X)
+        @test_throws ArgumentError OLS_data(randn(n+1), X)
+        params = ML(data)
+        @test params.B ≈ (X \ Y)
+        @test params.Σ ≈ sum(abs2, Y - X * (X \ Y)) / n
+        for _ in 1:10
+            β′ = randn(k)
+            Σ′ = abs(randn()) + 1
+            @test loglikelihood(data, OLS_params(β′, Σ′)) ≈
+                sum(logpdf.(Normal(0, √Σ′), Y - X * β′))
+        end
+    end
+end
+
+@testset "OLS matrixvariate" begin
+    m = rand(3:6)
+    k = rand(5:10)
+    n = rand(90:100) + k
+    A = randn(m, m)
+    X = randn(n, k)
+    B = randn(k, m)
+    Y = X * B + randn(n, m) * A
+    data = OLS_data(Y, X)
+    @test_throws ArgumentError OLS_data(randn(n+1, ), X)
+    params = ML(data)
+    @test params.B ≈ (X \ Y)
+    E = Y - X * (X \ Y)
+    @test params.Σ ≈ E'*E / n
+    for _ in 1:10
+        B′ = randn(k, m)
+        A′ = randn(m, m)
+        Σ′ = A′' * A′           # ensures PSD
+        @test loglikelihood(data, OLS_params(B′, Σ′)) ≈
+            loglikelihood(MvNormal(zeros(m), Σ′), (Y - X * B′)')
     end
 end
