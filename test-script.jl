5d9171980435bf2826f2aba446ece75f748625f9
Tamas K. Papp <tkpapp@gmail.com>
2018-01-07 13:59:54 +0100

    fix test script by changing order

--- test-script.jl
+++ test-script.jl
@@ -1,6 +1,6 @@
 # test script to be run on Travis
-Pkg.clone(pwd())
 # NOTE remove when ContinuousTransformations.jl is registered
 Pkg.clone("https://github.com/tpapp/ContinuousTransformations.jl.git")
+Pkg.clone(pwd())
 Pkg.build("IndirectLikelihood")
 Pkg.test("IndirectLikelihood"; coverage=true)
